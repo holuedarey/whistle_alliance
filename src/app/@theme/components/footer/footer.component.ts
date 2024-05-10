78d6357951530a3025cff143d9f391873ec80b6c
Samuel Ogunwole <holudare2076@gmail.com>
2024-05-10 14:04:42 +0100

    fixed issues

--- src/app/@theme/components/footer/footer.component.ts
+++ src/app/@theme/components/footer/footer.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.scss'],
   template: `
     <span class="caption">
-      Power by Whistles Alliance 2023
+      Powered by Whistles Alliance 2023
     </span>
   `,
 })
